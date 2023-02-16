a29390cda922ef02d97765e7d6f058f8d0f1ac48
Wasam-ul-hassan3010 <124611992+Wasam-ul-hassan3010@users.noreply.github.com>
2023-02-16 20:03:41 +0500

    Add files via upload

---
+++ Assignment4/app.js
@@ -0,0 +1,170 @@
+// CHAPTER 9 ANND 10 //
+
+// Question 01*
+// var city = prompt("Enter your city name")
+// if(city == "Karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+// Question 02*
+// var gender = prompt("Enter your gender")
+// if(gender == "male"){
+//     alert("Good Morning Sir.")
+// }
+// if(gender == "female"){
+//     alert("Good Morning Ma’am..")
+// }
+
+// Question 03*
+// var signalColor = prompt("Enter traffic signal color")
+// document.write("<table><tr><th>signal color</th><th>message</th></tr> <tr><td>Red</td><td>Must stop</td></tr> <tr><td>Yellow</td><td>Ready to move</td> <tr><td>Green</td><td>Move now</td></table>")
+
+// Questio 04*
+// var remaingFuel = prompt("Enter your fuel")
+// var currentFuel = "0.25liters"
+// if(remaingFuel !== currentFuel){
+//     alert('“Please refill the fuel in your car”')
+// }
+
+// Question 05*
+// .A
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// .B
+// var b = 82;
+// if (++b === 83){
+// alert("given condition for variable b is true");
+// }
+
+// .C
+// var c = 12;
+// if (++c === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (c++ < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// .D
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// .E
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("false");
+// }
+// // .F
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// Question 06*
+// var sub_1_ObtMarks = +prompt("Enter Subject 1 Obtained Marks");
+// var sub_1_TotalMarks = +prompt("Enter Subject 1 Total Marks");
+
+// var sub_2_ObtMarks = +prompt("Enter Subject 2 Obtained Marks");
+// var sub_2_TotalMarks = +prompt("Enter Subject 1 Total Marks");
+
+// var sub_3_ObtMarks = +prompt("Enter Subject 3 Obtained Marks");
+// var sub_3_TotalMarks = +prompt("Enter Subject 1 Total Marks");
+
+// var totalMarks = sub_1_TotalMarks + sub_2_TotalMarks + sub_3_TotalMarks;
+// var totalObtainedMarks = sub_1_ObtMarks + sub_2_ObtMarks + sub_3_ObtMarks;
+
+// var percentage = totalObtainedMarks / totalMarks * 100;
+
+// document.write ("<h1>Marks Sheet</h1>");
+// document.write ("Total marks: " + totalMarks + "<br>");
+// document.write ("Marks obtained: " + totalObtainedMarks + "<br>");
+// document.write ("Percentage: " + percentage + "%" + "<br>");
+
+// if (percentage >= 80) {
+//     document.write ("Grade: A-one" + "<br>");
+//     document.write ("Remarks: Excellent" + "<br>");
+// }
+
+// else if (percentage >= 70) {
+//     document.write ("Grade: A" + "<br>");
+//     document.write ("Remarks: Good" + "<br>");
+// }
+
+// else if (percentage >= 60) {
+//     document.write ("Grade: B" + "<br>");
+//     document.write ("Remarks: You need to improve" + "<br>");
+// }
+
+// else {
+//     document.write ("Grade: Fail" + "<br>");
+//     document.write ("Remarks: Sorry");
+// }
+
+
+// Question 07*
+// var secretNum = prompt("guess the secret number from 1 to 10")
+// if(secretNum == 5){
+//     alert("Bingo! Correct answer.")
+// }
+// else if(secretNum == 6){
+//     alert("Close enough to the correct answer.")
+// }
+
+// Question 08*
+// var number = prompt("Enter a num that divisible by 3")
+// if (number%3  == 0){
+//     alert("The number " + number + " is divisible by 3")
+// }
+// else{alert("The number " + number + " is not divisible by 3")}
+
+// Question 09*
+// var givenNumber = prompt("Give a number")
+// if(givenNumber == )
+
+// Question 10*
+// var temprature = prompt("Enter a temp")
+// if(temprature > 40){
+//     alert("“It is too hot outside.”")
+// }
+// else if(temprature > 30){
+//     alert("“The Weather today is Normal.”")
+// } 
+// else if(temprature > 20){
+//     alert("“Today’s Weather is cool.”")
+// }
+// else if(temprature > 10){
+//     alert("“OMG! Today’s Weather is so cool.”")
+// }
+
+// Question 11*
+// var firstNum = +prompt("Enter a first number")
+// var opt = prompt("Enter a opretor")
+// var secondNum = +prompt("Enter a second number")
+// var result;
+// if(opt == "+"){
+//     result = firstNum + secondNum
+// }
+// if(opt == "-"){
+//     result = firstNum - secondNum
+// }
+// if(opt == "*"){
+//     result = firstNum * secondNum
+// }
+// if(opt == "/"){
+//     result = firstNum / secondNum
+// }
+// if(opt == "%"){
+//     result = firstNum % secondNum
+// }
+// alert(result)
---
+++ Assignment4/main.js
@@ -0,0 +1,66 @@
+// Questio 01*
+// var userInputForUpperOrLowerCase =  prompt("Just type a character JS will tell you, it's in upper case or in lower case ");
+// var index = 0;
+// var asciiCode = userInputForUpperOrLowerCase.charCodeAt(index);
+// if (asciiCode >= 65 && asciiCode <= 90) {
+//     alert("UPPER CASE!! " +asciiCode)
+// }
+// else if (asciiCode >= 97 && asciiCode <= 122) {
+//     alert("lowercase!! " + asciiCode)
+// }
+// else {
+//     alert("not a letter!!" + asciiCode)
+// }
+
+// Question 02*
+// var num1 = prompt("Enter integer 1")
+// var num2 = prompt("Enter integer 2")
+// if(num1 > num2){
+//   alert(num1 + " is larger then " + num2)
+// }
+// else if(num2 > num1){
+//   alert(num2 + " is larger then " + num1)
+// }
+// else{alert("The two integer are equal")}
+
+// Questio 03*
+// var num = prompt("Enter a number")
+// if(num == 0){
+//     alert("The value is zero")
+// }
+// else if(num > 0){
+//     alert("The value is positive")
+// }
+// else if(num < 0){
+//     alert("The value is negative")
+// }
+
+
+// Question 04*
+// var character = prompt("Enter a alphabet")
+// if(character === "a" || character == "e" || character == "i" || character == "o" || character == "u"){ 
+//     alert("It's a true")
+// }
+// else{
+//     alert("It's a false")
+// }
+
+// Question 05*
+// var correctPassword = "Wasam123"
+// var userPassword = prompt("Enter your password")
+// if (userPassword === "") {
+//   alert("Please enter your password");
+// } else if (userPassword === correctPassword) {
+//   alert("Correct! The password you entered matches the original password.");
+// } else {
+//   alert("Incorrect password");
+// }
+
+// Question 06*
+// var greeting = prompt("");
+// var hour = 13;
+// if (hour < 18) {
+//   alert("Good day")}
+// else{
+//   alert("Good evening");
+// }
